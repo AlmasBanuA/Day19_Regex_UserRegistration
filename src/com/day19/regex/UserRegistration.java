a883ee30ad00c66db921ca2d4113542ea93cc561
[AlmasBanuA2011] <[almasbanu3304@gmail.com]>
2022-03-11 15:52:32 +0530

    Valid Mobile Number

--- src/com/day19/regex/UserRegistration.java
+++ src/com/day19/regex/UserRegistration.java
@@ -4,6 +4,7 @@ package com.day19.regex;
  * As a User needs to check the regex pattern for the firstName
  * As a User needs to check the regex pattern for the lastName 
  * As a User needs to check the regex pattern for the emailId 
+ * As a User needs to check the regex pattern for the MobileNumber
 */
 
 /**
@@ -64,11 +65,29 @@ public class UserRegistration {
 		/*
 		 * As a User needs to check the regex pattern for the emailId
 		 */
-		boolean check = Pattern.matches("([0-9A-Za-z])+([-+._][0-9A-Za-z]+)*" + "@([0-9A-Za-z])+[.]([a-zA-Z])+([.][A-Za-z]+)*", eMail);
+		boolean check = Pattern
+				.matches("([0-9A-Za-z])+([-+._][0-9A-Za-z]+)*" + "@([0-9A-Za-z])+[.]([a-zA-Z])+([.][A-Za-z]+)*", eMail);
 		if (check)
 			System.out.println("Valid");
 		else
 			System.out.println("Please Enter a Valid Email with Only \"example.abc@bl.co.in\" latter");
+	}
+
+	/**
+	 * creating method mobileNum() to check mobileNum is valid or not
+	 */
+	public void mobileNum() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Mobile number with country code (91)  : ");
+		String mobileNum = sc.nextLine();
+		/*
+		 * As a User needs to check the regex pattern for the MobileNumber
+		 */
+		boolean check = Pattern.matches("^[+0-9]{2}{10}$", mobileNum);
+		if (check)
+			System.out.println("Valid");
+		else
+			System.out.println("Please Enter a Valid Mobile Num ex:- 91 9090909090");
 	}
 
 	public static void main(String[] args) {
@@ -83,5 +102,6 @@ public class UserRegistration {
 		user.firstNameCheck();
 		user.lastNameCheck();
 		user.email();
+		user.mobileNum();
 	}
 }
