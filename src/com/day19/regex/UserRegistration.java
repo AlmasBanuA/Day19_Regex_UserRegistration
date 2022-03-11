47e35d3886199ec434b7b0e3e8d45224d8fe6118
AlmasBanuA <94681887+AlmasBanuA@users.noreply.github.com>
2022-03-11 15:32:35 +0530

    Valid FirstName

--- src/com/day19/regex/UserRegistration.java
+++ src/com/day19/regex/UserRegistration.java
@@ -1,5 +1,10 @@
 package com.day19.regex;
 
+/**
+  * UC1- As a User need to enter a valid First Name
+  * First name starts with Cap and has
+  * minimum 3 characters
+  */
 import java.util.Scanner;
 import java.util.regex.*;
 
