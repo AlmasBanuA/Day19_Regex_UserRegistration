f7cc5b9e019f9fdd12d8a2a12dda3d3b87af56bd
[AlmasBanuA2011] <[almasbanu3304@gmail.com]>
2022-03-11 16:26:52 +0530

    Valid Password of minimum 8 characters with atleat one upper case letter and atleat one numeric number

--- src/com/day19/regex/UserRegistration.java
+++ src/com/day19/regex/UserRegistration.java
@@ -7,6 +7,7 @@ package com.day19.regex;
  * As a User needs to check the regex pattern for the MobileNumber
  * As a User needs to check the regex pattern for Password rule1 minimum 8 characters
  * As a User needs to check the regex pattern for Password rule2 minimum 1 UperCase Letter
+ * As a User needs to check the regex pattern for Password rule3 which contains minimum 1 Numeric
 */
 
 /**
@@ -121,6 +122,22 @@ public class UserRegistration {
 			System.out.println("Please Enter a Valid password, it should have minimum 8 characters with 1 upper case");
 	}
 
+	/*
+	 * create method passwordRule3() which contains minimum 8 characters with 1
+	 * upper case and 1 Numeric
+	 */
+	public void passWordRule3() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter password :  ");
+		String passWord = sc.nextLine();
+		boolean check = Pattern.matches("[A-Z]{1}[a-z]{6,}[0-9]{1}", passWord);
+		if (check)
+			System.out.println("Valid");
+		else
+			System.out.println(
+					"Please Enter a Valid password, it should have minimum 8 characters with, 1 upper case and 1 Numeric ");
+	}
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		/*
@@ -136,5 +153,6 @@ public class UserRegistration {
 		user.mobileNum();
 		user.passWordRule1();
 		user.passWordRule2();
+		user.passWordRule3();
 	}
 }
